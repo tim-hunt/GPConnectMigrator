91e244035936ed0c2b6fdb8e7d8699c0e39a8a68
Tim Hunt <timothy.hunt@madetech.com>
2023-08-24 18:33:58 +0100

    Addition of address helper and new fields on patient

---
+++ GPMigratorApp/GPConnect/Helpers/AddressHelper.cs
@@ -0,0 +1,182 @@
+using Hl7.Fhir.Model;
+using Hl7.Fhir.ElementModel;
+
+namespace GPMigratorApp.GPConnect.Helpers
+{
+    public static class AddressHelper
+    {
+        public static string GenerateDisplayText(Address address)
+        {
+            List<string> displayTextLines = new List<string>();
+
+            if (address.Line.Any())
+            {
+                foreach (var line in address.Line)
+                {
+                    displayTextLines.Add(line);
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(address.City))
+            {
+                displayTextLines.Add(address.City);
+            }
+
+            if (!string.IsNullOrWhiteSpace(address.District))
+            {
+                displayTextLines.Add(address.District);
+            }
+
+            if (!string.IsNullOrWhiteSpace(address.PostalCode))
+            {
+                displayTextLines.Add(address.PostalCode);
+            }
+
+            return string.Join(", ", displayTextLines);
+        }
+
+        public static Address? FindHomeAddress(Patient fhirPatient)
+        {
+            if (!fhirPatient.Address.Any())
+            {
+                return null;
+            }
+
+            List<Address> homeAddresses = fhirPatient.Address
+                .Where(fhirAddress => fhirAddress.Use == null || fhirAddress.Use == Address.AddressUse.Home)
+                .ToList();
+
+            if (!homeAddresses.Any())
+            {
+                return null;
+            }
+
+            homeAddresses.Sort(CompareAddresses);
+
+            return homeAddresses.Last();
+        }
+        
+        public static IEnumerable<Address>? OtherAddresses(Patient fhirPatient)
+        {
+            if (!fhirPatient.Address.Any())
+            {
+                return null;
+            }
+
+            var homeAddresses = fhirPatient.Address.Where(x => !x.Matches(FindHomeAddress(fhirPatient)));
+
+            return homeAddresses;
+        }
+
+        private static int CompareAddresses(Address addr1, Address addr2)
+        {
+            // Ones without end dates are always later than ones with end dates
+            var end1 = addr1.End();
+
+            var end2 = addr2.End();
+
+
+            if (!end1.HasValue && !end2.HasValue)
+            {
+                // If neither has an end, then let the below sorting by start date apply
+            }
+            else if (end1.HasValue && !end2.HasValue)
+            {
+                // If 1 is ended but 2 is not, then 1 goes before 2 (null end counts as later)
+                return -1;
+            }
+            else if (!end1.HasValue && end2.HasValue)
+            {
+                // If 1 is not ended but 2 is, then 2 goes before 1
+                return 1;
+            }
+            else
+            {
+                // If both are ended, then sort by end date
+                if (end1 != null && end2 != null)
+                {
+                    var comp = DateTime.Compare(end1.Value, end2.Value);
+                    if (comp != 0)
+                    {
+                        return comp;
+                    }
+                }
+            }
+
+            var start1 = addr1.Start();
+ 
+            var start2 = addr2.Start();
+
+            if (!start1.HasValue && !start2.HasValue)
+            {
+                // If neither has a start date, then we can't sort any more
+                return 0;
+            }
+            else if (start1.HasValue && !start2.HasValue)
+            {
+                // If 1 has a date and 2 doesn't, then place 2 first (null start counts as earlier)
+                return 1;
+            }
+            else if (!start1.HasValue && start2.HasValue)
+            {
+                // If 1 doesn't have a start, but 2 does, then place 1 first
+                return -1;
+            }
+            else
+            {
+                // If both have starts, then it's a straight date comparison
+                return DateTime.Compare(start1.Value, start2.Value);
+            }
+        }
+
+        public static bool HasStart(this Address address)
+        {
+            return address.Period?.StartElement != null;
+        }
+        
+        public static bool HasEnd(this Address address)
+        {
+            return address.Period?.EndElement != null;
+        }
+        
+        public static DateTime? Start(this Address address)
+        {
+            var success = DateTime.TryParse(address.Period?.StartElement.Value, out var result);
+            if (success)
+                return result;
+            
+            return null;
+        }
+        
+        public static DateTime? End(this Address address)
+        {
+            var success = DateTime.TryParse(address.Period?.EndElement.Value, out var result);
+            if (success)
+                return result;
+            
+            return null;
+        }
+
+        public static string? FindCity(Patient fhirPatient)
+        {
+            var address = FindHomeAddress(fhirPatient);
+            return address?.City;
+        }
+
+        public static string? FindDistrict(Patient fhirPatient)
+        {
+            var address = FindHomeAddress(fhirPatient);
+            return address?.District;
+        }
+        
+        public static bool HasUse(Address toCheck, Address.AddressUse use) {
+
+            //when an address is ended it's use is changed to OLD, so compare use normally,
+            //but also let the use pass if either one of them is OLD
+            return toCheck.Use != null
+                   && (toCheck.Use == use
+                       || toCheck.Use == Address.AddressUse.Old
+                       || use == Address.AddressUse.Old);
+        }
+    }
+}
--- GPMigratorApp/GPConnect/Profiles/GPConnectPatient.cs
+++ GPMigratorApp/GPConnect/Profiles/GPConnectPatient.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using System.Runtime.Serialization;
+using GPMigratorApp.GPConnect.Helpers;
 using Hl7.Fhir.Introspection;
 using Hl7.Fhir.Model;
 
@@ -13,20 +14,45 @@ public class GPConnectPatient : Patient
     public GPConnectPatient(Patient patient)
     {
         InitInhertedProperties(patient);
+        
+        
     }
     public DateTime FirstRegistered
     {
         get
         {
-            var extension1 = this.Extension.FirstOrDefault(x => x.Url == "https://fhir.nhs.uk/STU3/StructureDefinition/Extension-CareConnect-GPC-RegistrationDetails-1");
-            var extension2 = (IEnumerable<Extension>) @extension1?.FirstOrDefault(x => x.Key == "extension").Value!;
-            var value1 = @extension2.FirstOrDefault()?.Value;
-            var value2 = (FhirDateTime)@value1?.FirstOrDefault(x => x.Key == "start").Value!;
-            var value3 = @value2?.Value;
-            return DateTime.Parse(@value3!);
+            var registration = this.Extension.FirstOrDefault(x => x.Url == "https://fhir.nhs.uk/STU3/StructureDefinition/Extension-CareConnect-GPC-RegistrationDetails-1");
+            var ext1 = registration?.Extension.FirstOrDefault(x => x.Url == "registrationPeriod");
+            var registered = (Period) ext1?.Value;
+            return DateTime.Parse(registered?.Start);
+        }
+    }
+
+    public Identifier? NHSNumber
+    {
+        get
+        {
+            var nhsNumber = Identifier.FirstOrDefault(x => x.System == "https://fhir.nhs.uk/Id/nhs-number");
+            return nhsNumber;
         }
     }
     
+    public Coding? NHSNumberVerified
+    {
+        get
+        {
+            var verified = NHSNumber?.Extension.FirstOrDefault(x => x.Url == "https://fhir.nhs.uk/STU3/StructureDefinition/Extension-CareConnect-GPC-NHSNumberVerificationStatus-1");
+            var value = (CodeableConcept) verified?.Value;
+            return value?.Coding.FirstOrDefault();
+          
+        }
+    }
+
+    public Address? HomeAddress => AddressHelper.FindHomeAddress(this);
+    
+    public IEnumerable<Address>? OtherAddresses => AddressHelper.OtherAddresses(this);
+
+
     private void InitInhertedProperties (object patient)
     {
         foreach (var propertyInfo in patient.GetType().GetProperties())
